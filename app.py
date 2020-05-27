e4fefd4c236ead6e007d1dd07d8a38c9706af030
Lu Yihui <lu_yihui@126.com>
2020-05-27 17:38:09 +0800

    car_list & statistic_vehicle

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 
 # 5500
@@ -19,7 +19,7 @@ def render_html(html_name):
 @app.route('/json/car_list')
 def car_list_get():
     # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
-    response = requests.get('http://127.0.0.1:5000/json/statistic_history')
+    response = requests.get('http://127.0.0.1:5000/json/car_list')
     print(response.json())
     return jsonify(response.json())
 
@@ -27,5 +27,19 @@ def car_list_get():
 def statistic_history_get():
     # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
     response = requests.get('http://127.0.0.1:5000/json/statistic_history')
+    print(response.json())
+    return jsonify(response.json())
+
+@app.route('/json/statistic_history_vehicle')
+def statistic_history_vehicle_get():
+    # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
+    startTime = request.args.get("startTime")
+    endtime = request.args.get("endtime")
+    spanType = request.args.get("type")
+    vin = request.args.get("vin")
+    print(startTime+endtime+spanType+vin)
+    print(request.args)
+
+    response = requests.get('http://127.0.0.1:5000/json/statistic_history_vehicle', params={'startTime':startTime,'endtime':endtime,'type':spanType,'vin':vin})
     print(response.json())
     return jsonify(response.json())
