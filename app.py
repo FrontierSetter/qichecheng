48ab8db55235a42acc14a08665bf517f26627044
Lu Yihui <lu_yihui@126.com>
2020-05-27 17:54:45 +0800

    change to server env

--- app.py
+++ app.py
@@ -19,14 +19,17 @@ def render_html(html_name):
 @app.route('/json/car_list')
 def car_list_get():
     # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
-    response = requests.get('http://127.0.0.1:5000/json/car_list')
+    response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
     print(response.json())
     return jsonify(response.json())
 
 @app.route('/json/statistic_history')
 def statistic_history_get():
     # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
-    response = requests.get('http://127.0.0.1:5000/json/statistic_history')
+    startTime = request.args.get("startTime")
+    endtime = request.args.get("endtime")
+    spanType = request.args.get("type")
+    response = requests.get('http://10.55.8.52:8860/api/v1/statistic/history', params={'startTime':startTime,'endtime':endtime,'type':spanType})
     print(response.json())
     return jsonify(response.json())
 
@@ -37,9 +40,9 @@ def statistic_history_vehicle_get():
     endtime = request.args.get("endtime")
     spanType = request.args.get("type")
     vin = request.args.get("vin")
-    print(startTime+endtime+spanType+vin)
-    print(request.args)
+    # print(startTime+endtime+spanType+vin)
+    # print(request.args)
 
-    response = requests.get('http://127.0.0.1:5000/json/statistic_history_vehicle', params={'startTime':startTime,'endtime':endtime,'type':spanType,'vin':vin})
+    response = requests.get('http://10.55.8.52:8860/api/v1/statistic/history/vehicle', params={'startTime':startTime,'endtime':endtime,'type':spanType,'vin':vin})
     print(response.json())
     return jsonify(response.json())
