6c7972eacb67b4a5a896bd4a7e3768743c7ec886
Lu Yihui <lu_yihui@126.com>
2020-05-27 19:48:25 +0800

    exit rate

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import requests
 import os
 
 # 5500
+#  $env:FLASK_ENV = "development"
 
 app = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -31,15 +32,20 @@ def render_favicon():
 
 @app.route('/json/car_list')
 def car_list_get():
-    # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
     print(routeEnv+'/vehicle/list')
     response = requests.get(routeEnv+'/vehicle/list')
     print(response.json())
     return jsonify(response.json())
 
+@app.route('/json/statistic_history_exit')
+def history_exit_get():
+    response = requests.get(routeEnv+'/statistic/history/exit')
+    print(response.json())
+    return jsonify(response.json())
+
+
 @app.route('/json/statistic_history')
 def statistic_history_get():
-    # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
     starttime = request.args.get("starttime")
     endtime = request.args.get("endtime")
     spanType = request.args.get("type")
@@ -49,14 +55,10 @@ def statistic_history_get():
 
 @app.route('/json/statistic_history_vehicle')
 def statistic_history_vehicle_get():
-    # response = requests.get('http://10.55.8.52:8860/api/v1/vehicle/list')
     starttime = request.args.get("starttime")
     endtime = request.args.get("endtime")
     spanType = request.args.get("type")
     vin = request.args.get("vin")
-    # print(startTime+endtime+spanType+vin)
-    # print(request.args)
-
     response = requests.get(routeEnv+'/statistic/history/vehicle', params={'starttime':starttime,'endtime':endtime,'type':spanType,'vin':vin})
     print(response.json())
     return jsonify(response.json())
